bb7fb77658de98c175881695fcc77a74059967c7
Pinan11233 <chaubinhan11233@gmail.com>
2023-07-18 00:29:44 +0700

    upload source code backend

---
+++ routes/reviews.js
@@ -0,0 +1,10 @@
+
+import express from 'express'
+import { createReview } from './../controllers/reivewController.js'
+import { verifyUser } from '../utils/verifyToken.js'
+
+const router = express.Router()
+
+router.post('/:tourId', verifyUser, createReview)
+
+export default router
